173145ff64608ee76ac3ef2e0bb4f2993ccfc682
srikanthreddy741 <bandarisrikanth741@gmail.com>
2022-07-14 16:08:26 +0530

    added Employee Attendence

--- EmployeeManagement/EmployeeManagement/Program.cs
+++ EmployeeManagement/EmployeeManagement/Program.cs
@@ -1,3 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("EMPLOYEE MANAGEMENT SYSTEM");
 
+int IS_FULL_TIME = 1;
+Random random = new Random();
+int empCheck = random.Next(0, 2);
+if (empCheck == IS_FULL_TIME)
+{
+    Console.WriteLine("Employee is Present");
+}
+else
+{
+    Console.WriteLine("Employee is Absent");
+}
