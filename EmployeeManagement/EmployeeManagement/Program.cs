e6571e4af351ada0c1e277dc3a2838bfe5d13096
srikanthreddy741 <bandarisrikanth741@gmail.com>
2022-07-14 16:26:12 +0530

    added EmployeeWage

--- EmployeeManagement/EmployeeManagement/Program.cs
+++ EmployeeManagement/EmployeeManagement/Program.cs
@@ -1,14 +1,20 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("EMPLOYEE MANAGEMENT SYSTEM");
 
+
 int IS_FULL_TIME = 1;
+int EMP_RATE_PER_HOUR = 20;
+int empHrs = 0;
+int empWage = 0;
 Random random = new Random();
 int empCheck = random.Next(0, 2);
 if (empCheck == IS_FULL_TIME)
 {
-    Console.WriteLine("Employee is Present");
+    empHrs = 8;
 }
 else
 {
-    Console.WriteLine("Employee is Absent");
+    empHrs = 0;
 }
+empWage = empHrs * EMP_RATE_PER_HOUR;
+Console.WriteLine("Emp Wage : " + empWage);
