86387f5d9cc438a36a614c90a27969e5c386ad13
srikanthreddy741 <bandarisrikanth741@gmail.com>
2022-07-14 20:10:49 +0530

    added parttime&wage

--- EmployeeManagement/EmployeeManagement/Program.cs
+++ EmployeeManagement/EmployeeManagement/Program.cs
@@ -1,14 +1,24 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("EMPLOYEE MANAGEMENT SYSTEM");
 
-int IS_FULL_TIME = 1;
+int IS_PART_TIME = 1;
+int IS_FULL_TIME = 2;
+int EMP_RATE_PER_HOUR = 20;
+int empHrs = 0;
+int empWage = 0;
 Random random = new Random();
-int empCheck = random.Next(0, 2);
-if (empCheck == IS_FULL_TIME)
+int empCheck = random.Next(0, 3);
+if (empCheck == IS_PART_TIME)
 {
-    Console.WriteLine("Employee is Present");
+    empHrs = 4;
+}
+else if (empCheck == IS_FULL_TIME)
+{
+    empHrs = 8;
 }
 else
 {
-    Console.WriteLine("Employee is Absent");
+    empHrs = 0;
 }
+empWage = empHrs * EMP_RATE_PER_HOUR;
+Console.WriteLine("Emp Wage : " + empWage);
