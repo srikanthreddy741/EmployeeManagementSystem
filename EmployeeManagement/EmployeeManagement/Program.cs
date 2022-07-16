3fa295c377479b9d30bd6681d7373cca2d297d1e
srikanthreddy741 <bandarisrikanth741@gmail.com>
2022-07-16 23:19:21 +0530

    added class method

--- EmployeeManagement/EmployeeManagement/Program.cs
+++ EmployeeManagement/EmployeeManagement/Program.cs
@@ -1,14 +1,44 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("EMPLOYEE MANAGEMENT SYSTEM");
+class program
+{
+    public const int IS_PART_TIME = 1;
+    public const int IS_FULL_TIME = 2;
+    public const int EMP_RATE_PER_HOUR = 20;
+    public const int NUM_OF_WORKING_DAYS = 20;
+    public const int MAX_HRS_IN_MONTH = 100;
+    public static int companyWage()
+    {
+        int empHrs = 0;
+        int totalEmpHrs = 0;
+        int totalWorkingDays = 0;
+        while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS)
+        {
+            totalWorkingDays++;
+            Random random = new Random();
+            int empCheck = random.Next(0, 3);
+            switch (empCheck)
+            {
+                case IS_PART_TIME:
+                    empHrs = 4;
+                    break;
+                case IS_FULL_TIME:
+                    empHrs = 8;
+                    break;
+                default:
+                    empHrs = 0;
+                    break;
+            }
+            totalEmpHrs += empHrs;
+            Console.WriteLine("Day#:" + totalWorkingDays + " Emp Hrs : " + empHrs);
+        }
+        int totalEmpWage = totalEmpHrs * EMP_RATE_PER_HOUR;
+        Console.WriteLine("Total Emp Wage : " + totalEmpWage);
+        return totalEmpWage;
+    }
 
-int IS_FULL_TIME = 1;
-Random random = new Random();
-int empCheck = random.Next(0, 2);
-if (empCheck == IS_FULL_TIME)
-{
-    Console.WriteLine("Employee is Present");
-}
-else
-{
-    Console.WriteLine("Employee is Absent");
+    static void main(string[] args)
+    {
+        companyWage();
+    }
 }
