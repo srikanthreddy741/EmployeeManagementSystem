e3f437de87b4d97ecc552c3a1844211030c5e406
srikanthreddy741 <bandarisrikanth741@gmail.com>
2022-07-20 16:28:58 +0530

    added multiple companies

---
+++ EmployeeManagement/EmployeeManagement/EmpWage.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeManagement
+{
+    internal class EmpWage
+    {
+        public const int IS_PART_TIME = 1;
+        public const int IS_FULL_TIME = 2;
+
+        public static int companyEmpWage(string company, int empRatePerHour, int numOfWorkingDays, int maxHoursPerMonth)
+        {
+            int empHrs = 0, totalEmpHrs = 0, totalWorkingDays = 0;
+
+            while (totalEmpHrs <= maxHoursPerMonth && totalWorkingDays < numOfWorkingDays)
+            {
+                totalWorkingDays++;
+                Random random = new Random();
+                int empCheck = random.Next(0, 3);
+                switch (empCheck)
+                {
+                    case IS_PART_TIME:
+                        empHrs = 4;
+                        break;
+                    case IS_FULL_TIME:
+                        empHrs = 8;
+                        break;
+                    default:
+                        empHrs = 0;
+                        break;
+                }
+                totalEmpHrs += empHrs;
+                Console.WriteLine("Day#:" + totalWorkingDays + " Emp Hrs : " + empHrs);
+            }
+            int totalEmpWage = totalEmpHrs * empRatePerHour;
+            Console.WriteLine("Total Emp Wage for company : " + company + "is: " + totalEmpWage);
+            return totalEmpWage;
+        }
+
+    }
+}
--- EmployeeManagement/EmployeeManagement/Program.cs
+++ EmployeeManagement/EmployeeManagement/Program.cs
@@ -1,43 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("EMPLOYEE MANAGEMENT SYSTEM");
-class program
-{
-    public const int IS_PART_TIME = 1;
-    public const int IS_FULL_TIME = 2;
-   
-  public static int companyEmpWage(string company, int empRatePerHour, int numOfWorkingDays, int maxHoursPerMonth)
-    {
-        int empHrs = 0 , totalEmpHrs = 0 , totalWorkingDays = 0;
-        
-        while (totalEmpHrs <= maxHoursPerMonth && totalWorkingDays < numOfWorkingDays)
-        {
-            totalWorkingDays++;
-            Random random = new Random();
-            int empCheck = random.Next(0, 3);
-            switch (empCheck)
-            {
-                case IS_PART_TIME:
-                    empHrs = 4;
-                    break;
-                case IS_FULL_TIME:
-                    empHrs = 8;
-                    break;
-                default:
-                    empHrs = 0;
-                    break;
-            }
-            totalEmpHrs += empHrs;
-            Console.WriteLine("Day#:" + totalWorkingDays + " Emp Hrs : " + empHrs);
-        }
-        int totalEmpWage = totalEmpHrs * empRatePerHour;
-        Console.WriteLine("Total Emp Wage for company : " + company + "is: " + totalEmpWage);
-        return totalEmpWage;
-    }
+﻿using EmployeeManagement;
 
-    static void main(string[] args)
-    {
-        companyEmpWage("DMart",20,2,10);
-        companyEmpWage("Reliance",10,4,20);
-
-    }
-}
+EmpWage.companyEmpWage("DMart",20,2,10);
+EmpWage.companyEmpWage("Reliance",10,4,20);
