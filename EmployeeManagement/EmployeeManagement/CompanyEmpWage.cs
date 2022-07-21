dd1a7514a2ffab5f04a0e69ab59e65bc1a3556f4
srikanthreddy741 <bandarisrikanth741@gmail.com>
2022-07-21 20:57:17 +0530

    added CompanyEmpWageBuilderArray

---
+++ EmployeeManagement/EmployeeManagement/CompanyEmpWage.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeManagement
+{
+    public class CompanyEmpWage
+    {
+        public string company;
+        public int empRatePerHour;
+        public int numOfWorkingDays;
+        public int maxHoursPerMonth;
+        public int totalEmpWage;
+
+        public CompanyEmpWage(string company, int empRatePerHour, int numOfWorkingDays, int maxHoursPerMonth)
+        {
+            this.company = company;
+            this.empRatePerHour = empRatePerHour;
+            this.numOfWorkingDays = numOfWorkingDays;
+            this.maxHoursPerMonth = maxHoursPerMonth;
+        }
+
+        public CompanyEmpWage(string company, int empRatePerHour, int numOfWorkingDays, int maxHoursPerMonth, int totalEmpWage)
+        {
+            this.company = company;
+            this.empRatePerHour = empRatePerHour;
+            this.numOfWorkingDays = numOfWorkingDays;
+            this.maxHoursPerMonth = maxHoursPerMonth;
+            this.totalEmpWage = totalEmpWage;
+        }
+
+        public void setTotalEmpWage(int totalEmpWage)
+        {
+            this.totalEmpWage = totalEmpWage;
+        }
+
+        public string tostring()
+
+        {
+            return "Total Emp Wage For compaany : " + this.company + " is " + this.totalEmpWage;
+        }
+    }
+}
--- EmployeeManagement/EmployeeManagement/Program.cs
+++ EmployeeManagement/EmployeeManagement/Program.cs
@@ -1,14 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("EMPLOYEE MANAGEMENT SYSTEM");
+﻿using EmployeeManagement;
+class Program
+{
 
-int IS_FULL_TIME = 1;
-Random random = new Random();
-int empCheck = random.Next(0, 2);
-if (empCheck == IS_FULL_TIME)
-{
-    Console.WriteLine("Employee is Present");
-}
-else
-{
-    Console.WriteLine("Employee is Absent");
+    static void Main(string[] args)
+    {
+        EmpWageBuilderArray empWageBuilder = new EmpWageBuilderArray();
+        empWageBuilder.addCompanyEmpWage("Dmart", 20, 2, 10);
+        empWageBuilder.addCompanyEmpWage("Reliance", 10, 4, 20);
+        empWageBuilder.computeEmpWage();
+        
+    }
 }
